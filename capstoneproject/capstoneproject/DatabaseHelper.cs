c9e020b32348b23057cabcd4fe54f5efd5976e5e
wonyLaptop <wonyLaptop@DESKTOP-UP4P85H>
2019-08-15 12:36:07 +0900

    DatabaseHelper 추가

---
+++ capstoneproject/capstoneproject/DatabaseHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capstoneproject
+{
+    class DatabaseHelper
+    {
+        //if DB파일이 없으면 예외처리는 주석으로 비워두기
+        //else open DB
+
+        //if DB에 테이블이 없으면 Create Table(Default/공용)
+        //else ok.
+        
+        //SQL Query을 실행시키는 구문 (Return값은 쿼리 결과)
+    }
+}

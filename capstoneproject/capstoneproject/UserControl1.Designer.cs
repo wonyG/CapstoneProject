dd3b39065fdeeec04704b5f876bf1435a13a4aeb
wonyLaptop <wonyLaptop@DESKTOP-UP4P85H>
2019-08-15 12:10:58 +0900

    디자인 추가

--- capstoneproject/capstoneproject/UserControl1.Designer.cs
+++ capstoneproject/capstoneproject/UserControl1.Designer.cs
@@ -38,79 +38,87 @@
             this.button8 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listView1 = new System.Windows.Forms.ListView();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(682, 26);
+            this.button1.Location = new System.Drawing.Point(661, 26);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(96, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "검색";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(682, 55);
+            this.button2.Location = new System.Drawing.Point(661, 55);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(96, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "태그 추가";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(682, 84);
+            this.button3.Location = new System.Drawing.Point(661, 84);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(96, 23);
             this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
+            this.button3.Text = "목록에서 제거";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(682, 113);
+            this.button4.Location = new System.Drawing.Point(661, 113);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.Size = new System.Drawing.Size(96, 23);
             this.button4.TabIndex = 3;
-            this.button4.Text = "button4";
+            this.button4.Text = "태그 관리";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(682, 207);
+            this.button5.Location = new System.Drawing.Point(661, 207);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.Size = new System.Drawing.Size(96, 23);
             this.button5.TabIndex = 4;
-            this.button5.Text = "button5";
+            this.button5.Text = "이곡 반복재생";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(682, 236);
+            this.button6.Location = new System.Drawing.Point(661, 236);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.Size = new System.Drawing.Size(96, 23);
             this.button6.TabIndex = 5;
-            this.button6.Text = "button6";
+            this.button6.Text = "랜덤재생";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Button6_Click);
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(682, 265);
+            this.button7.Location = new System.Drawing.Point(661, 265);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.Size = new System.Drawing.Size(96, 23);
             this.button7.TabIndex = 6;
-            this.button7.Text = "button7";
+            this.button7.Text = "전체 반복재생";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Button7_Click);
             // 
             // button8
             // 
-            this.button8.Location = new System.Drawing.Point(682, 348);
+            this.button8.Location = new System.Drawing.Point(661, 348);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.Size = new System.Drawing.Size(96, 23);
             this.button8.TabIndex = 7;
-            this.button8.Text = "button8";
+            this.button8.Text = "설정";
             this.button8.UseVisualStyleBackColor = true;
             // 
             // label1
@@ -129,20 +137,28 @@
             this.textBox1.Size = new System.Drawing.Size(461, 21);
             this.textBox1.TabIndex = 9;
             // 
-            // listBox1
+            // panel1
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 12;
-            this.listBox1.Location = new System.Drawing.Point(50, 55);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(530, 316);
-            this.listBox1.TabIndex = 10;
+            this.panel1.Location = new System.Drawing.Point(50, 409);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(707, 217);
+            this.panel1.TabIndex = 11;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(50, 55);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(529, 315);
+            this.listView1.TabIndex = 12;
+            this.listView1.UseCompatibleStateImageBehavior = false;
             // 
             // UserControl1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button8);
@@ -172,6 +188,7 @@
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListView listView1;
     }
 }
--- capstoneproject/capstoneproject/UserControl1.cs
+++ capstoneproject/capstoneproject/UserControl1.cs
@@ -10,6 +10,11 @@ using System.Windows.Forms;
 
 namespace capstoneproject
 {
+    /// <summary>
+    /// 패널에 필요한것: PictureBox 1개, Label 6개, Button 5개, 슬라이드 1개
+    /// </summary>
+    
+    
     public partial class UserControl1: UserControl
     {
         public UserControl1()
@@ -20,6 +25,45 @@ namespace capstoneproject
         private void UserControl1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //TextBox에 있는 텍스트를 가져와서 Tag검색
+            //SQL 쿼리문을 이용해서 DB에서 검색
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            //UserControl2로 이동하는 버튼
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            //listbox에서 선택된 곡을 제거하는 버튼
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            //UserControl3로 이동하는 버튼
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            //listbox에서 선택한 한 곡을 반복재생 하는 버튼
+            //or 현재 재생중인 곡을 반복재생 하는 버튼
+        }
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            //listbox 내부에 있는 모든 곡을 랜덤하게 재생하는 버튼
+            //random()함수값/listbox 내부에 있는 개체의 갯수를 계산해서 재생하면 될듯
+        }
+
+        private void Button7_Click(object sender, EventArgs e)
+        {
+            //listbox 내부에 있는 모든 곡을 순차적으로 반복재생하는 버튼
+            //if listbox.length < count, count = 0 를 사용하면 될듯
         }
     }
 }
